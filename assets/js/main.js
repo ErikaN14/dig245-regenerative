a932310ba54ac6ec0a8b28ba0c07b85b53a17581
Erika Navarro <erika072002@gmail.com>
2023-10-30 11:32:13 -0400

    API implementation (cont.) (10/30/2023)
    Working on API Authorization

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,119 @@ async function getData(url) {
 	);
 	console.log(data);
 });
+
+// Code Verifier (authorization code Step 1):
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+const codeVerifier  = generateRandomString(64);
+
+// Code Challenge (authorization code Step 2):
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+// Code Challenge (authorization code Step 3):
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+// Code Challenge (authorization code Step 4):
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
+
+//Requesting User Authorization (authorization code step 5):
+const clientId = 'YOUR_CLIENT_ID';
+const redirectUri = 'http://localhost:8080';
+
+const scope = 'user-read-private user-read-email';
+const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+// generated in the previous step
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: clientId,
+  scope,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: redirectUri,
+}
+
+authUrl.search = new URLSearchParams(params).toString();
+window.location.href = authUrl.toString();
+
+//Response (authorization code step 6):
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+
+//Request Access token (authroization code step 7):
+const getToken = async code => {
+
+  // stored in the previous step
+  let codeVerifier = localStorage.getItem('code_verifier');
+
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      code_verifier: codeVerifier,
+    }),
+  }
+
+  const body = await fetch(url, payload);
+  const response =await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+}
+
+//Request Authtoization (Client Credentials Flow Step 1):
+var client_id = 'CLIENT_ID';
+var client_secret = 'CLIENT_SECRET';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
+
+//Testing Access Token code from previous
+async function getProfile(accessToken) {
+  let accessToken = localStorage.getItem('access_token');
+
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }
+  });
+
+  const data = await response.json();
+}
+
+
