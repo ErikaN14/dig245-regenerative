eeeaa2fd15692afc0ee3f73de5cffed6d721f774
Erika Navarro <erika072002@gmail.com>
2023-10-30 15:39:52 -0400

    Debugged track function (10/30/2023)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /* javascript */
 
 // For each track there is an image URL
-  //Create a const variable that is equal to the image url and a function that allows to display the image
+//Create a const variable that is equal to the image url and a function that allows to display the image
 
 //Began adding AI code
 
@@ -12,45 +12,59 @@ var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
-}
+};
 
-// async version 
+// async version
 async function getData(url) {
-	let options = {
-		method: "GET",
-		headers: {
-			"Content-type": "application/json",
-			Authorization:
+  let options = {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+      Authorization:
         //Update token after every hour!
-				"Bearer BQAy_nqlhqv6lzcVlZ2aOrevEg736CW52p1yWZPQ6p4r2WVFJT4ArT_S8gtEzIFXGFnqswwuMzVPz5kY4Z8IGv5JJzGS-S5e2F7nJrq1tgwl_L77X9E"
-		}
-	};
+        "Bearer BQDh2tvlA-uyd_cSHP9p8MwBgQgRkUV79Jt5JrSNJ6Y5kWLYg4g7SIELhH39UGzc1IT7jq6Fxggz-sent3m4credstdA3AG5OVdIHbVEjk_cjcMk3m4",
+    },
+  };
 
-	// request external resource
-	fetch(url, options)
-		// the entire response object
-		.then((response) => {
-			// console.log(response);
-			// -> 200, "OK"
-			console.log(response.status, response.statusText);
-			// parse response.body (convert to JSON), pass to next
-			return response.json();
-		})
-		.then((data) => {
-			// data = the deserialized data of the external file
-			console.log(data); // log the object
-		});
+  // request external resource
+  fetch(url, options)
+    // the entire response object
+    .then((response) => {
+      // console.log(response);
+      // -> 200, "OK"
+      console.log(response.status, response.statusText);
+      // parse response.body (convert to JSON), pass to next
+      return response.json();
+    })
+    .then((data) => {
+      // data = the deserialized data of the external file
+      console.log(data); // log the object
+    })
+	.catch((err) =>{
+console.warn(err)
+	});
 }
 // artist
 // let data = getData("https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb");
 
 (async function () {
-	// search
-	let range = "1955-1960";
-	let data = await getData(
-		`https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl`
-	);
-	console.log(data);
-});
+  console.log(123);
+  // search
+  let range = "1955-1960";
+  
+// range 
+// let data = await getData(
+// 	`https://api.spotify.com/v1/search?q=artist:Miles%20Davis%20year:${range}&type=album`
+// );
+
+
+//  track
+  let data = await getData(
+    `https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl`
+  );
+
+
+  console.log(data);
+})();
