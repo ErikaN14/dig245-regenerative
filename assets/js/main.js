7c312f0066f20f3102c26c2d219c8783446267c8
Erika Navarro <erika072002@gmail.com>
2023-11-01 14:31:46 -0400

    API is producing images
    Images of albums based on decade chosen!

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,9 @@ output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
+  if (output.innerHTML != this.value){
+    updateTrack(slider.value);
+  }
   output.innerHTML = this.value;
 };
 
@@ -28,12 +31,13 @@ async function getData(url) {
       "Content-type": "application/json",
       Authorization:
         //Update token after every hour!
-        "Bearer BQClrDi2qnAP2SpsMMCnlCcRwO4kbZGFHY2PvxqbL3hdGXAvR8OHvH-d0rhGzcaDNuMSruj0XPrpZBhD1MwjjD4e3K-3y8N5ZrSzQOkj05GCFzIY2x8",
+        "Bearer BQBRC9X2JhtWeZD3LPO2Q2qI3r4JHzoRXqr4t2pSkMYnpr7LbAcIypOOLnzoYbNVwHm1fsZvCUz0_-U55uFSg547bKOJ5XgNwgwiq07y9g_BP7hGhO8",
     },
   };
 
   // request external resource
-  return fetch(url, options)
+  return (
+    fetch(url, options)
       // the entire response object
       .then((response) => {
         // console.log(response);
@@ -50,44 +54,54 @@ async function getData(url) {
       .catch((err) => {
         console.warn(err);
       })
- // );
-}
-
-//to get new random tracks each time
-function getRandomSearch() {
-  // A list of all characters that can be chosen.
-  const characters = "abcdefghijklmnopqrstuvwxyz";
-
-  // Gets a random character from the characters string.
-  const randomCharacter = characters.charAt(
-    Math.floor(Math.random() * characters.length)
   );
-  let randomSearch = "";
-
-  // Places the wildcard character at the beginning, or both beginning and end, randomly.
-  switch (Math.round(Math.random())) {
-    case 0:
-      randomSearch = randomCharacter + "%";
-      break;
-    case 1:
-      randomSearch = "%" + randomCharacter + "%";
-      break;
-  }
-
-  return randomSearch;
+  // );
 }
-const randomOffset = Math.floor(Math.random() * 10000);
 
-(async function () {
+// //to get new random tracks each time
+// function getRandomSearch() {
+//   // A list of all characters that can be chosen.
+//   const characters = "abcdefghijklmnopqrstuvwxyz";
+
+//   // Gets a random character from the characters string.
+//   const randomCharacter = characters.charAt(
+//     Math.floor(Math.random() * characters.length)
+//   );
+//   let randomSearch = "";
+
+//   // Places the wildcard character at the beginning, or both beginning and end, randomly.
+//   switch (Math.round(Math.random())) {
+//     case 0:
+//       randomSearch = randomCharacter + "%";
+//       break;
+//     case 1:
+//       randomSearch = "%" + randomCharacter + "%";
+//       break;
+//   }
+
+//   return randomSearch;
+// }
+// const randomOffset = Math.floor(Math.random() * 10000);
+
+async function updateTrack(year) {
   //defining range (is it needed?)
-  let range = output + (output + 9); //output = decade chosen on slider (adding 9 to go from start of decade to end of decade)
+  let lastYear =  Number(year) + 9; //output = decade chosen on slider (adding 9 to go from start of decade to end of decade)
 
   //  track
   let data = await getData(
-    `https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl` //create function that randomizes the letters and numbers for end of track link
+    //`https://api.spotify.com/v1/search/11dFghVXANMlKmJXsNCbNl` //create function that randomizes the letters and numbers for end of track link
+    `https://api.spotify.com/v1/search?q=year%3A${year}-${lastYear}&type=track&market=US&offset=0`
   );
 
-  let date = data.album.release_date;
+  console.log(data);
+  let track = data.tracks.items[Math.floor(Math.random() * data.tracks.items.length)]; //random track value!
+  
+  console.log(track);
+  const trackImage = track.album.images[0].url;
+
+  document.querySelector(".song_image").innerHTML = `<img src="${trackImage}">`
+
+return;
 
   // Trying to get just the year in which the track was released
   let trackYear = date[0];
@@ -98,9 +112,14 @@ const randomOffset = Math.floor(Math.random() * 10000);
   //console.log(output.innerHTML.slice(0, 3)); //decade choasen on slider by user
 
   //checking to see if year track was released is within decade chosen on slider
+  //console.log('123');
+  //console.log(data.album.images[0].url);
   if (trackYear.slice(0, 3) === output.innerHTML.slice(0, 3)) {
-    // const trackImage = 
+    const trackImage = data.album.images[0].url; //double check syntax
+    //would use this in HTML to link song to image
+    console.log("Test");
   } else {
-    //generate a new set of tracks to look at
+    console.log(trackYear.slice(0, 3) === output.innerHTML.slice(0, 3));
+    //generate a new set of tracks to look at (have a function for this but it is not working)
   }
-})();
+};
