7d98d6c65332b9e77866e58db5be5bc5161dc36b
Erika Navarro <erika072002@gmail.com>
2023-10-31 23:30:02 -0400

    Working on API Implementation
    Figuring out how to randomize tracks given by Spotify API instead of just printing the same tracks on the terminal each time

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,10 @@
 
 //Began adding API code
 
+//Questions to ask:
+	// How to randomize tracks given on terminal (new tracks each time)?
+	// If track is not within decade needed, how to refresh track set given to produce a new set of tracks?
+
 // All code is coming from W3 Schools
 // Link: https://www.w3schools.com/howto/howto_js_rangeslider.asp
 var slider = document.getElementById("myRange");
@@ -14,7 +18,11 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
   output.innerHTML = this.value;
+  //<button onclick="myFunction()">Click me</button>
 };
+// object.onclick = function () {
+//   myScript;
+// };
 
 // async version
 async function getData(url) {
@@ -24,74 +32,83 @@ async function getData(url) {
       "Content-type": "application/json",
       Authorization:
         //Update token after every hour!
-        "Bearer BQD8FXW_x2vgjQIi_cdlmMzwqR9HUxs0plOh9-NwfnpMER4Zw16G6HtqkxOGJtANIZe3pN4xR8EKZ3BqtRlnktYG98K0N9E-n6_nRwwi6vUh4PUuYo4",
+        "Bearer BQClrDi2qnAP2SpsMMCnlCcRwO4kbZGFHY2PvxqbL3hdGXAvR8OHvH-d0rhGzcaDNuMSruj0XPrpZBhD1MwjjD4e3K-3y8N5ZrSzQOkj05GCFzIY2x8",
     },
   };
 
   // request external resource
-  return fetch(url, options)
-    // the entire response object
-    .then((response) => {
-      // console.log(response);
-      // -> 200, "OK"
-      console.log(response.status, response.statusText);
-      // parse response.body (convert to JSON), pass to next
-      return response.json();
-    })
-    .then((data) => {
-      // data = the deserialized data of the external file
-    //   console.log(data); // log the object
-	return data;
-    })
-	.catch((err) =>{
-console.warn(err)
-	});
+  return (
+    fetch(url, options)
+      // the entire response object
+      .then((response) => {
+        // console.log(response);
+        // -> 200, "OK"
+        console.log(response.status, response.statusText);
+        // parse response.body (convert to JSON), pass to next
+        return response.json();
+      })
+      .then((data) => {
+        // data = the deserialized data of the external file
+        //   console.log(data); // log the object
+        return data;
+      })
+      .catch((err) => {
+        console.warn(err);
+      })
+  );
 }
-// artist
-// let data = getData("https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb");
+
+//to get new random tracks each time
+function getRandomSearch() {
+	// A list of all characters that can be chosen.
+	const characters = 'abcdefghijklmnopqrstuvwxyz';
+	
+	// Gets a random character from the characters string.
+	const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+	let randomSearch = '';
+  
+	// Places the wildcard character at the beginning, or both beginning and end, randomly.
+	switch (Math.round(Math.random())) {
+	  case 0:
+		randomSearch = randomCharacter + '%';
+		break;
+	  case 1:
+		randomSearch = '%' + randomCharacter + '%';
+		break;
+	}
+  
+	return randomSearch;
+  }
+  const randomOffset = Math.floor(Math.random() * 10000);
+
 
 (async function () {
-  //console.log(123);
-  // search
-
   //defining range
-  let range = output + (output+9); //output = decade chosen on slider (adding 9 to go from start of decade to end of decade)
+  let range = output + (output + 9); //output = decade chosen on slider (adding 9 to go from start of decade to end of decade)
 
-//  track
+  //  track
   let data = await getData(
-    `https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl`
+    `https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl` //create function that randomizes the letters and numbers for end of track link
   );
 
-	let date = data.album.release_date;
+  let date = data.album.release_date;
 
-	// Trying to get just the year in which the track was released
-	let year = date[0];
-	for (let i = 1; i <= 3; i++){
-		year = year + date[i];
-		//console.log(year);
-	}
-	console.log(year);
-	console.log(data);
+  // Trying to get just the year in which the track was released
+  let trackYear = date[0];
+  for (let i = 1; i <= 3; i++) {
+    trackYear = trackYear + date[i];
+  }
+  console.log(trackYear.slice(0, 3)); //at this point, I have the year in which the track was released
+  console.log(output.innerHTML.slice(0, 3));
 
-	 for(let j = output; j <= 9; i++){ //what data type is value? (try to figure this out!!!)
-		console.log(j);
-		console.log("test");
-		if(year === output){
-			console.log(year === output);
-		 }
-		//  else{
-		// 	console.log("This is false!")
-		//  }
-	 }
-
-
-
-  
-  
-
-
-//   console.log(data);
-  //console.log(data);
-  //console.log(data.album.release_date);
-  //console.log(date[0]);
+  //checking to see if year track was released is within decade chosen on slider
+  if (trackYear.slice(0, 3) === output.innerHTML.slice(0, 3)) {
+    //console.log(trackYear[0,3] === sliderYear[0,3]);
+    //track year is within decade chosen on slider
+    //add track to webpage
+  } else {
+    console.log("false!");
+    //track year is not within decade chosen on slider
+    //find another track
+  }
 })();
